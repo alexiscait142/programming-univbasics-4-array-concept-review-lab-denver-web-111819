4468a787a8812eb4ec65f4b39739f8fc109e9b6f
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-09 19:22:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,9 +5,9 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  puts array.max
+  p array.max
 end
 
 def find_min_value(array)
-  puts array.min
+  p array.min
 end
