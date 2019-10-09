b5f4433a77bf6c76c2add61a26d2355f31e311d0
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-09 19:02:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,5 @@
 def find_element_index(array, value_to_find)
- while array.include?(value_to_find) === true do
-    return array.index(value_to_find)
+ array.index 
 end
 
 def find_max_value(array)
